4808ddc60bbd54787388ffd73955adc6de8ce027
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-03-01 01:21:12 +0600

    added search phone file now

--- js/phone-bazar.js
+++ js/phone-bazar.js
@@ -1,13 +1,32 @@
 const loadPhones = () => {
-    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+
+    searchField.value = '';
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
         .then(res => res.json())
-        .then(data => displayPhones(data))
+        .then(data => displaySearchResult(data.data))
+
 }
 
-const displayPhones = result => {
-    const phones = result.data;
-    const phonesDiv = document.getElementById('phones');
-    for (const phone of phones) {
-        console.log(phone)
-    }
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(info => {
+        console.log(info);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100 mt-5">
+                <img src="${info.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">${info.phone_name}</h4>
+                    <p class="card-text fw-bold">${info.brand}</p>
+                    <button class="bg-success ps-2 pe-2 rounded text-white">Explore</button>
+                </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
