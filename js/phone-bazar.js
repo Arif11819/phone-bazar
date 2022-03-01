a401bdf5d7a502f18e04f712cfb0bb7d3d3cf05e
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-03-01 16:08:32 +0600

    add file now

--- js/phone-bazar.js
+++ js/phone-bazar.js
@@ -1,9 +1,9 @@
 const loadPhones = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-
+    // clear data
     searchField.value = '';
-
+    // load data
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
@@ -12,23 +12,30 @@ const loadPhones = () => {
 }
 
 const displaySearchResult = data => {
-    const searchResult = document.getElementById('search-result');
-    data.forEach(info => {
-        // console.log(info);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card h-75 w-75 mx-auto mt-5">
-                <img src="${info.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h4 class="card-title">${info.phone_name}</h4>
-                    <p class="card-text fw-bold">${info.brand}</p>
-                    <button onclick="loadPhoneDetail('${info.slug}')" class="bg-success ps-2 pe-2 rounded text-white">Explore</button>
-                </div>
-        </div>
-        `;
-        searchResult.appendChild(div);
-    })
+    if (data.length <= 20) {
+        const searchResult = document.getElementById('search-result');
+        searchResult.textContent = '';
+        data.forEach(info => {
+            // console.log(info);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-75 w-75 mx-auto mt-5">
+                    <img src="${info.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">${info.phone_name}</h4>
+                        <p class="card-text fw-bold">${info.brand}</p>
+                        <button onclick="loadPhoneDetail('${info.slug}')" class="bg-success ps-2 pe-2 rounded text-white">Explore</button>
+                    </div>
+            </div>
+            `;
+            searchResult.appendChild(div);
+        })
+    }
+    else {
+        const searchResult = document.getElementById('search-result').style.display = 'none';
+    }
+
 }
 
 const loadPhoneDetail = phoneId => {
@@ -44,10 +51,13 @@ const phoneDetail = info => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-        <img src="${info.image}" class="card-img-top w-25 mx-auto" alt="...">
-        <div class="card-body mx-auto">
-             <h5 class="card-title">${info.name}</h5>
-             <p class="card-text">${info.releaseDate}</p>
+        <img src="${info.image}" class="card-img-top" alt="...">
+        <div class="card-body mx-auto ms-0">
+             <h5 class="card-title fw-bold">${info.name}</h5>
+             <p class="card-text">ReleaseDate : ${info.releaseDate ? info.releaseDate : 'No relese date found!!'}</p>
+             <p class="card-text">Sensors : ${info.mainFeatures.sensors}</p>
+             <p class="card-text">Storage : ${info.mainFeatures.storage}</p>
+             <p class="card-text">Others : ${info.others.WLAN}</p>
         </div>
     `;
     phoneDetail.appendChild(div);
